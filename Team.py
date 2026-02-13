fd0be096979cbb031621eb39257de30fae8a0dac
Nafyad Guye <nafyadamanguye@gmail.com>
2026-02-13 15:43:58 +0300

    Refactor input handling and counting logic

--- Team.py
+++ Team.py
@@ -1,11 +1,7 @@
 n=int(input())
-counter=0
-matrix= []
+count=0
 for i in range(n):
-    row=list(map(int,input().split()))
-    matrix.append(row)
-    
-for j in matrix:
-    if j[0]+j[1]+j[2] >=2:
-        counter+=1
-print(counter)
+    lis=list(map(int, input().split()))
+    if sum(lis)>=2:
+        count+=1
+print(count)
