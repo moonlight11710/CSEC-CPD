6e405f971e10064fda4a73dfa97d2ed51ae0cdc2
Nafyad Guye <nafyadamanguye@gmail.com>
2026-02-13 22:06:03 +0300

    Update Beautiful Matrix.py

--- Beautiful Matrix.py
+++ Beautiful Matrix.py
@@ -7,7 +7,7 @@ for i in range(5):
 for i in matrix:
     for j in i:
         if j==1:
-            print(abs(3-(matrix.index(i)+1))+abs(3-(i.index(j)+1)))
+            print(abs(2-(matrix.index(i)))+abs(2-(i.index(j))))
             break
           
     
