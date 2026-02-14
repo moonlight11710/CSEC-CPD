fa2b0ed2875d60b442da2a45f3169517e0b2e6ec
Nafyad Guye <nafyadamanguye@gmail.com>
2026-02-14 11:55:07 +0300

    Update Stones on the Table.py

--- Stones on the Table.py
+++ Stones on the Table.py
@@ -1,13 +1,11 @@
 n=int(input())
-lis=input()
-counter=0
-
-for i in range(n):
-    if len(lis)==0 or len(lis)== 1:
-        break
-    elif lis[0]==lis[1]:
-        counter+=1
-        lis=lis[1:]
+word=input()
+checker=word[0]
+count=0
+for i in range(1, len(word)):
+    if word[i]==checker:
+        count+=1
     else:
-        lis=lis[1:]
-print(counter)
+        checker=word[i]
+print(count)
+
