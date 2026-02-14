24a876c9228d1996fb701d5ba9c035a3ab9e8488
Nafyad Guye <nafyadamanguye@gmail.com>
2026-02-14 10:17:14 +0300

    Refactor gender checker logic using a set

--- Boy or Girl.py
+++ Boy or Girl.py
@@ -1,12 +1,5 @@
-inp=input()
-checker=[]
-for i in range(len(inp)):
-    if inp[i] in checker:
-        continue
-    else:
-        checker.append(inp[i])
-
-if len(checker)%2==0:
+name=set(input())
+if len(name)%2==0:
     print("CHAT WITH HER!")
 else:
     print("IGNORE HIM!")
