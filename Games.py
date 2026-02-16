3be8ce5ce286ff557ac6966040052161b6d9af0f
Nafyad Guye <nafyadamanguye@gmail.com>
2026-02-16 08:46:08 +0300

    Refactor game logic and input storage
    Refactored game input handling and logic to count matches.

--- Games.py
+++ Games.py
@@ -1,17 +1,13 @@
 n=int(input())
-liss=[]
-numberofgames=n*(n-1)
-counter=0
-
+home=[]
+away=[]
+answer=0
 for i in range(n):
-    lis=list(map(int,input().split()))
-    liss.append(lis)
-
+    row=list(map(int, input().split()))
+    home.append(row[0])
+    away.append(row[1])
 for i in range(n):
     for j in range(n):
-        if i==j:
-            continue
-        elif liss[i][0]==liss[j][1]:
-            counter+=1
-            
-print(counter)
+        if home[i]==away[j]:
+            answer+=1
+print(answer)
